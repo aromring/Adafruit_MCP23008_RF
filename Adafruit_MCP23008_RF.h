c264bd0db59113a9183da33928d3ce492b1e1685
Robert Fraczkiewicz <aromring@gmail.com>
2016-09-10 16:53:29 -0700

    Added customized Adafruit library

--- Adafruit_MCP23008_RF.h
+++ Adafruit_MCP23008_RF.h
@@ -11,6 +11,8 @@
   BSD license, all text above must be included in any redistribution
  ****************************************************/
 
+//  Modified the original class name, Adafruit_MCP23008, to avoid conflicts with
+//  the Adafruit's LCD library
 //  Copy this file into Arduino't libraries/Adafruit_MCP23008_RF folder
 
 #ifndef _Adafruit_MCP23008_RF_H
